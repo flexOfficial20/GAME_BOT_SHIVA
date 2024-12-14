715624ca7e98d862d87cb88e9ac3c8934ebd4250
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-12-14 10:50:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ import random
 import config
 
 # Client setup
-app = Client("seize", bot_token=config.BOT_TOKEN)
+app = Client(
+    "seize", 
+    api_id=config.API_ID, 
+    api_hash=config.API_HASH, 
+    bot_token=config.BOT_TOKEN
+)
 
 # MongoDB connection
 # Client 1 collection DB URL
