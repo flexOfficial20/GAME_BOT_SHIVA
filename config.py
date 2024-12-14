df85b28f441aad56b0d58f11cd9ced650c864e3a
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-12-14 09:12:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import re
 from os import getenv
 
 #-------------------------------------------------------
-API_ID = int(getenv(""))
-API_HASH = getenv("")
+API_ID = int(getenv("24089031"))
+API_HASH = getenv("0615e3afe13ddaaf8e9ddbd3977d35ff")
 
 #---------------------------------------------------------
-BOT_TOKEN = getenv("")
+BOT_TOKEN = getenv("7771917183:AAH-AEqm3VJa1S5BChBNdWivCC0LVlGAoLk")
 # --------------------------------------------------------
 OWNER_ID = int(getenv("6806897901"))
 #--------------------------
@@ -18,7 +18,7 @@ UPLOAD_CHAT_ID = int(getenv("-1002339161004"))
 #--------------------------------
 LOGGER_ID = int(getenv("-1002342285813"))
 #--------------------------------------------------------
-MONGO_DB_URI = getenv("")
+MONGO_DB_URI = getenv("mongodb+srv://shivaiaxz:YfVBSatihAQlPAXc@cluster0.rqcpm.mongodb.net/?retryWrites=true&w=majority")
 #---------------------------------------------------------
 #update coin mongodb url
 MONGO_DB_UPDATATE_URI = getenv("mongodb+srv://shivaiaxz:YfVBSatihAQlPAXc@cluster0.rqcpm.mongodb.net/?retryWrites=true&w=majority")
